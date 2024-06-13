3b4b4f90d31b50ceb6e91d74f56707dd258de471
bijoymamud <bijoymamud.09@gmail.com>
2024-06-13 08:48:23 +0600

    setting up project layout with routes

---
+++ src/Pages/Home/Cards/Cards.jsx
@@ -0,0 +1,18 @@
+
+
+const Cards = () => {
+  return (
+    <div>
+      <h2 className='text-7xl font-extrabold'>All cards will show here</h2>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Cards from "../Cards/Cards";
+
+const Home = () => {
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
+};
+
+export default Home;
