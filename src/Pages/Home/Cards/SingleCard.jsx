e038a48360d9c4ef4386d2d639b792d972135657
bijoymamud <bijoymamud.09@gmail.com>
2024-06-13 12:06:20 +0600

    implement search section

--- src/Pages/Home/Cards/SingleCard.jsx
+++ src/Pages/Home/Cards/SingleCard.jsx
@@ -1,25 +1,5 @@
 
 
-// const SingleCard = ({ item, onBuyNow }) => {
-//   const { id, model_name, img, price } = item;
-//   return (
-//     <section>
-//       <div className="card w-full bg-base-100 shadow-xl">
-//         <figure><img className="h-[250px] w-full" src={img} alt="laptop" /></figure>
-//         <div className="card-body">
-//           <h2 className="card-title">{model_name}</h2>
-//           <p>${price}</p>
-//           <div className="card-actions justify-end">
-//             <button className="btn btn-sm" onClick={() => onBuyNow(id)}>Buy Now</button>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default SingleCard;
-
 const SingleCard = ({ item, onBuyNow }) => {
   const { id, model_name, img, price } = item;
   return (
@@ -30,7 +10,7 @@ const SingleCard = ({ item, onBuyNow }) => {
         </figure>
         <div className="card-body">
           <h2 className="card-title">{model_name}</h2>
-          <p>${price}</p>
+          <p>$ {price}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-sm" onClick={onBuyNow}>Buy Now</button>
           </div>
