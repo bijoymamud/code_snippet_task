3bbb0f863b21e4d5ba778583f7c470eb151fd51b
bijoymamud <bijoymamud.09@gmail.com>
2024-08-14 00:26:04 +0600

    navbar design

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -2,9 +2,52 @@ const Navbar = () => {
 
 
   return (
-    <div>
-      <h2>This is navbar</h2>
-    </div>
+    <section className="bg-gray-100 min-h-screen p-5">
+      
+      {/* navbar section start */}
+      <div className="bg-white flex items-center justify-between p-3 rounded-lg" >
+
+          <div className="flex items-center gap-2  rounded-lg  ">
+
+          <div className="avatar">
+              <div className="w-12 rounded-full">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+          </div>
+
+          <div >
+            <h2 className="text-sm ">Al Mamud Bijoy</h2>
+            <p className="text-sm text-gray-400" >bijoymamud4@gmail.com</p>
+          </div>
+          </div>
+
+          <div className="w-2/4">
+              <label className="input input-bordered flex items-center gap-2">
+                  
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                    fill="currentColor"
+                    className="h-4 w-4 opacity-70">
+                    <path
+                      fillRule="evenodd"
+                      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                      clipRule="evenodd" />
+                  </svg>
+                  <input type="text" className="grow" placeholder="Search a snippet" />
+            </label>
+          </div>
+
+          <div>
+          <input type="checkbox" className="toggle" defaultChecked />
+
+          </div>
+
+      </div>
+      {/* navbar section start */}
+
+
+    </section>
   );
 };
 
